252bf4d54f4b1a4addda7884ca200e4fc0ca2d38
Nathan Nowack <thrast36@gmail.com>
2024-02-21 23:12:20 -0600

    better logging

--- main.py
+++ main.py
@@ -5,44 +5,54 @@ import threading
 from datetime import datetime
 import redis
 
-r = redis.Redis(host="localhost", port=6379, db=0)
+from viz import process_messages, update_visuals
+
 # docker run -p 6379:6379 -d redis:latest
+r = redis.Redis(host="localhost", port=6379, db=0)
+
+AUTOCLAIM_COUNT = 100
+MIN_IDLE_TIME = 10_000
+
+def pprint(msg: str):
+    update_visuals(
+        thread_name=threading.current_thread().name,
+        message=msg
+    )
 
 def ensure_consumer_group_exists(stream_name, groupname):
     try:
         # implcitly uses id="$"
         r.xgroup_create(name=stream_name, groupname=groupname, mkstream=True)
+        
     except redis.exceptions.ResponseError as e:
         if "BUSYGROUP Consumer Group name already exists" not in str(e):
             raise
 
 
 def submit_event(task_key, event_data):
-    """
-    Submit an event to a stream based on the task_key.
-    """
     stream_name = f"stream:{task_key}"
-    event_id = r.xadd(stream_name, {"data": json.dumps(event_data)})
-    print(f"Event added to {stream_name} with ID {event_id}")
+    event_id = r.xadd(stream_name, {"data": json.dumps(event_data)}).decode("utf-8")
+    pprint(f"submitting entry ... {stream_name}:{event_id}")
+
 
 
 def event_submission_thread():
     """
     Thread function to submit events in random bursts.
     """
-    task_keys = ["task1", "task2"]  # Example task keys
+    task_keys = ["task1", "task2"]
+    
     while True:
         sleep_time = random.randint(1, 5)
-        print(f"SUBMISSION THREAD: Sleeping for {sleep_time} seconds...")
+        pprint(f"SUBMISSION THREAD: Sleeping for {sleep_time} seconds...")
         time.sleep(sleep_time)
 
         # Submit a burst of events
-        burst_size = random.randint(1, 5)
-        for _ in range(burst_size):
+        for _ in range(random.randint(1, 5)):
             task_key = random.choice(task_keys)
             event_data = {
                 "timestamp": str(datetime.now()),
-                "detail": f"{task_key} event",
+                "detail": f"some event for {task_key}",
             }
             submit_event(task_key, event_data)
 
@@ -54,43 +64,36 @@ def main_loop(client_name):
             ensure_consumer_group_exists(
                 stream_name=stream_name, groupname="my_consumer_group"
             )
-
-            # Use XAUTOCLAIM to handle pending messages
-            start_id = '0-0'  # Initial start ID for XAUTOCLAIM
+            start_id = '0-0'  # start from the beginning
             while True:
-                result = r.xautoclaim(
+                result = r.xautoclaim( # https://redis.io/commands/xautoclaim/
                     name=stream_name,
                     groupname="my_consumer_group",
                     consumername=client_name,
-                    min_idle_time=10000,  # TODO: how to choose this number?
+                    min_idle_time=MIN_IDLE_TIME,
                     start_id=start_id,
-                    count=100  # TODO: how to choose this number?
+                    count=AUTOCLAIM_COUNT,
                 )
-                next_start_id = result[0]
-                claimed_messages = result[1]
+                next_start_id, claimed_messages = result[0], result[1]
 
                 if not claimed_messages:
-                    break  # if no more messages to claim
+                    break
                 
-                # Process and acknowledge claimed messages
                 for _, msg_data in claimed_messages:
-                    # Process message
                     data_str = msg_data[b"data"].decode("utf-8")
                     data_obj = json.loads(data_str)
-                    print(f"Claimed: sending to client from {stream_name}: {data_obj}")
-                # Acknowledge all claimed messages at once
-                message_ids = [msg_id for msg_id, _ in claimed_messages]
-                if message_ids:
+                    pprint(f"Claimed and processed: {stream_name}: {data_obj.get('detail')}") # noqa
+
+                if message_ids := [msg_id for msg_id, _ in claimed_messages]:
                     r.xack(stream_name, "my_consumer_group", *message_ids)
 
                 # Prepare for the next iteration if needed
                 start_id = next_start_id
 
-            # Reading new messages from the stream using xreadgroup
             messages = r.xreadgroup(
                 groupname="my_consumer_group",
                 consumername=client_name,
-                streams={stream_name: ">"},
+                streams={stream_name: ">"},  # `>` ~= msgs not seen by consumer group
                 count=5, # TODO: how to choose this number?
                 block=1000, # TODO: how to choose this number?
             )
@@ -99,16 +102,16 @@ def main_loop(client_name):
             new_msg_ids = []
             for _, msgs in messages:
                 for msg_id, msg_data in msgs:
+                    i = msg_id.decode("utf-8")
                     data_str = msg_data[b"data"].decode("utf-8")
                     data_obj = json.loads(data_str)
-                    print(
-                        f"Processing (and maybe ack) from {stream_name}: {data_obj}"
-                    )
+
                     
                     if random.random() < 0.8: # ack 80% of the time
-                        new_msg_ids.append(msg_id)
+                        pprint(f"Processing + ack for {stream_name}:{i}: {data_obj.get('detail')}") # noqa
+                        new_msg_ids.append(i)
                     else:
-                        print(f"Ope! Not acking {msg_id}")
+                        pprint(f"Ope! Not acking {stream_name}:{i}: {data_obj.get('detail')}") # noqa
             
             if new_msg_ids:
                 r.xack(stream_name, "my_consumer_group", *new_msg_ids)
@@ -118,15 +121,21 @@ def main_loop(client_name):
 
 
 def start_simulation():
-    submission_thread = threading.Thread(target=event_submission_thread, daemon=True)
+    client_name = "foo-client"
+    threading.Thread(target=process_messages, daemon=True).start()
+    submission_thread = threading.Thread(
+        target=event_submission_thread,
+        daemon=True,
+        name=client_name
+    )
     submission_thread.start()
 
-    main_loop("client1")
+    main_loop(client_name)
 
 
 if __name__ == "__main__":
     try:
         start_simulation()
     except KeyboardInterrupt:
-        print("Exiting...")
+        print("\n\nExiting...")
         exit(0)
---
+++ viz.py
@@ -0,0 +1,45 @@
+from queue import Queue
+from rich.console import Console
+from rich.panel import Panel
+from rich.layout import Layout
+
+console = Console()
+layout = Layout()
+
+N_MESSAGES = 50
+client_name = "foo-client"
+
+layout.split_row(
+    Layout(name="main_thread"),
+    Layout(name="submission_thread")
+)
+layout["main_thread"].update(Panel("", title="Main Thread Log"))
+layout["submission_thread"].update(Panel("", title="Submission Thread Log"))
+
+queue = Queue()
+
+logs = {"MainThread": [], client_name: []}
+
+def process_messages():
+    while True:
+        thread_name, message = queue.get()
+        
+        if len(logs[thread_name]) >= N_MESSAGES:
+            logs[thread_name].pop(0)
+        logs[thread_name].append(message)
+        
+        layout["main_thread"].update(
+            Panel("\n".join(logs["MainThread"]), title="Main Thread Log")
+        )
+        layout["submission_thread"].update(
+            Panel("\n".join(logs[client_name]), title="Client Thread Log")
+        )
+
+        console.clear()
+        console.print(layout, end="/r")
+        
+        queue.task_done()
+
+
+def update_visuals(thread_name, message):
+    queue.put((thread_name, message))
